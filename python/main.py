bf72a812a4cfd612c4e0791a810f087383f0f213
rshimaok <rihs14804@gmail.com>
2023-05-11 22:15:13 +0900

    3-2 done

--- python/main.py
+++ python/main.py
@@ -1,6 +1,7 @@
 import os
 import logging
 import pathlib
+import json
 from fastapi import FastAPI, Form, HTTPException
 from fastapi.responses import FileResponse
 from fastapi.middleware.cors import CORSMiddleware
@@ -22,8 +23,23 @@ app.add_middleware(
 def root():
     return {"message": "Hello, world!"}
 
+#curl -X POST --url 'http://localhost:9000/items' -d 'name=jacket' -d 'category=fashion'
 @app.post("/items")
-def add_item(name: str = Form(...)):
+def add_item(name: str = Form(...),category: str = Form(...)):
+    item = {"name": name, "category": category}
+
+    #import json 必要
+    #items.jsonを開く
+    f = open('items.json', 'r')
+    #開いたファイルをjsonとして読み込む
+    data = json.load(f)
+    #読み込んだjsonにitemを追加
+    data["items"].append(item)
+    #書き込むファイルを開く
+    f = open('items.json', 'w')
+    #jsonを書き込む(辞書型のデータ)    
+    json.dump(data, f)
+
     logger.info(f"Receive item: {name}")
     return {"message": f"item received: {name}"}
 
